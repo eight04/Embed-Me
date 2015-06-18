2907178b6d657701952674a5046da27a3e82962f
eight <eight04@gmail.com>
2015-06-18 16:36:25 +0800

    Update GM_config

--- embed-me.user.js
+++ embed-me.user.js
@@ -10,7 +10,7 @@
 // @namespace   eight04.blogspot.com
 // @description An userscript to embed video, images from links.
 // @include     http*
-// @require     https://greasyfork.org/scripts/7212-gm-config-eight-s-version/code/GM_config%20(eight's%20version).js?version=56964
+// @require     https://greasyfork.org/scripts/7212-gm-config-eight-s-version/code/GM_config%20(eight's%20version).js?version=57385
 // @grant       GM_addStyle
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
