e8321e4672d360e1cfa3449598ff2db62773e677
eight04 <eight04@gmail.com>
2023-10-23 21:47:04 +0800

    Fix: twitch

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,8 @@
 import fumen from "./fumen.js";
-import gfycat from "./gfycat.js";
 import image from "./image.js";
 import imgur from "./imgur.js";
 import soundcloud from "./soundcloud.js";
 import twitch from "./twitch.js";
 import video from "./video.js";
 import youtube from "./youtube.js";
-export default [fumen,gfycat,image,imgur,soundcloud,twitch,video,youtube];
+export default [fumen,image,imgur,soundcloud,twitch,video,youtube];
